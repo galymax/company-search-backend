52f8bd1ab6e9713e206ef459b1ab551639ff3bb0
galymax <werkhowski@gmail.com>
2022-04-16 15:23:43 +0200

    get specialities

---
+++ controllers/companies.js
@@ -0,0 +1,23 @@
+import Company from '../models/Company.js';
+
+// filter by name and specialities
+async function getFilteredCompanies(req, res) {
+  const { name, specialities } = req.query;
+  const query = {};
+  if (name) {
+    query.name = new RegExp(name, 'i');
+  }
+  if (specialities) {
+    query.specialities = { $in: specialities.split(',') };
+  }
+  const companies = await Company.find(query);
+  res.json(companies);
+}
+
+// get list of available specialities
+async function getSpecialities(req, res) {
+    const specialities = await Company.distinct('specialities');
+    res.json(specialities);
+}
+
+export { getFilteredCompanies, getSpecialities };
